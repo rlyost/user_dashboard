a0155981d36305389e3ae8e2fd35d86e6e1a41aa
rlyost <ry@ryost.us>
2018-03-24 22:10:11 -0700

    working on last page...the wall

--- apps/userdash/models.py
+++ apps/userdash/models.py
@@ -1,7 +1,78 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import bcrypt
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
+class UserManager(models.Manager):
+    # EMAIL validation
+    def email_validator(self, postData):
+        errors = {}
+        # Validates email address for proper format.
+        if len(postData['email']) < 1:
+            errors["email"] = "Email cannot be blank!"
+            return errors
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors["email"] = "Invalid Email Address!"
+            return errors
+
+    # Checks to see if email is already in Db
+    def dbcheck_validator(self, postData):
+        errors = {}
+        #grabs record if email exists in the Db
+        mail_check = User.objects.filter(email=postData['email'])
+        if len(mail_check) == 0:
+            errors["email"] = "Email not found!"
+            return errors
+
+    # Db DUPLICATE validation
+    def db_validator(self, postData):
+        errors = {}
+        mail_check = User.objects.filter(email=postData['email'])
+        if len(mail_check) != 0:
+            dup_check = User.objects.filter(email=postData['email']).id
+            if dup_check != int(postData['id']):
+                errors["email"] = "Duplicate address in database, please enter another address."
+                return errors
+
+    # NAME validation    
+    def name_validator(self, postData):
+        errors = {}
+        # Checks name fields
+        if len(postData['fname']) < 2 or len(postData['lname']) < 2:
+            errors["name"] = "Name must be longer."
+            return errors
+        elif not str(postData['fname']).isalpha():
+            errors["name"] = "First Name can only be letters."
+            return errors       
+        elif not str(postData['lname']).isalpha():
+            errors["name"] = "Last Name can only be letters."
+            return errors 
+
+    # PASSWORD validation
+    def pw_validator(self, postData):
+        errors = {}
+        # Check password length and matching confirmation
+        if len(postData['password']) < 8:
+            errors["password"] = "Password is not long enough!"
+            return errors
+        elif postData['password'] != postData['password2']:
+            errors["password"] = "Passwords do not match!"
+            return errors
+
+    # HASHED PASSWORD validation
+    def hpw_validator(self, postData):
+        errors = {}
+        if len(postData['password']) > 7:
+            log_pass = User.objects.get(email=postData['email']).password
+        else:
+            errors['email'] = "Please provide a valid password."
+            return errors
+        #password validation - compares hashed logged in password to hashed Db password
+        if not bcrypt.checkpw(postData['password'].encode(), log_pass.encode()):
+            errors["email"] = "Password does not match."
+            return errors
+   
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -12,12 +83,18 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+    objects = UserManager()
+
 class Post(models.Model):
     message = models.CharField(max_length=500)
+    user = models.ForeignKey(User, related_name="poster")
+    users = models.ManyToManyField(User, related_name="posts")
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
 class Comment(models.Model):
     comments = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, related_name="commented")
+    user = models.ForeignKey(User, related_name="comments")
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
--- apps/userdash/views.py
+++ apps/userdash/views.py
@@ -2,112 +2,210 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from time import gmtime, strftime
-from .models import User    
+from .models import *    
 import bcrypt
-import re
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 # HOME *************************************************
 
 def index(request):
     return render(request, 'userdash/index.html')
 
-# REGISTRATION *************************************************
+# SIGN-IN PAGE ******************************************
+
+def signin(request):
+    return render(request, 'userdash/signin.html')
+
+# REGISTRATION PAGE ******************************************
 
 def register(request):
-    if request.method == "POST":
-        #grabs record if email exists in the Db
-        reg_check = User.objects.filter(email=request.POST['regemail'])
+    return render(request, 'userdash/register.html')
+
+# REGISTRATION VALIDATION AND RECORDING ***********************
+
+def register_val(request):
+     if request.method == "POST":
         #hashes password for storage to the Db        
-        hashed_password = bcrypt.hashpw(request.POST['regpassword'].encode(), bcrypt.gensalt(13))
-
+        hashed_password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt(13))
         request.session['fname'] = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['regemail']
-        password = request.POST['regpassword']
-        password2 = request.POST['regpassword2']
-    
-        # Checks name fields
-        if len(request.session['fname']) < 2 or len(lname) < 2:
-            messages.warning(request, "Name must be longer.")
+        errors = User.objects.name_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
             return redirect('reg')
-        elif not str(request.session['fname']).isalpha():
-            messages.warning(request, "First Name can only be letters.")
-            return redirect('reg')       
-        elif not str(lname).isalpha():
-            messages.warning(request, "Last Name can only be letters.")
+        errors = User.objects.email_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
             return redirect('reg')
-      
-        # Validates email address for proper format.
-        if len(email) < 1:
-            messages.warning(request, "Email cannot be blank!")
+        errors = User.objects.db_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
             return redirect('reg')
-        elif not EMAIL_REGEX.match(email):
-            messages.warning(request, "Invalid Email Address!")
+        errors = User.objects.pw_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
             return redirect('reg')
-        elif len(reg_check) != 0:
-            messages.warning(request, "Duplicate address, please enter another one or try logging in!")
-            return redirect('reg')
-        
-        # Check password length and matching confirmation
-        if len(password) < 8:
-            messages.warning(request, "Password is not long enough!")
-            return redirect('reg')
-        elif password != password2:
-            messages.warning(request, "Passwords do not match!")
-            return redirect('reg')
-        password = ''
-        password2 = ''
-
-        request.session['from'] = 0
-        #creates a new validated record in database for new user
-        User.objects.create(first_name=request.session['fname'], last_name=lname, email=email, password=hashed_password)
-        #sets logged in user for use later
-        request.session['id'] = User.objects.last().id
-        request.session['from'] = 0
-        #redirects to success to display sucessful login
-    return redirect('dash')
+        else:
+            #creates a new validated record in database for new user
+            user = User.objects.create(first_name=request.session['fname'], last_name=request.POST['lname'], email=request.POST['email'], password=hashed_password, level=request.POST['level'])
+            #sets permission indicator
+            if request.session['id'] != 1:
+                    request.session['level'] = 0
+                    #sets logged in user for use later
+                    request.session['id'] = user.id
+            #redirects to success to display sucessful login
+            return redirect('dash')
 
 # SIGNIN *************************************************
 
-def signin(request):
+def signin_val(request):
     if request.method == "POST":
-        #Checks Db for log email address. If not there returns empty list
-        log_email = request.POST['logemail']
-        log_check = User.objects.filter(email=log_email)
-        
-        # Validates email address for proper format.
-        if len(log_email) < 1:
-            messages.warning(request, "Email cannot be blank!")
-            return redirect('/')
-        elif not EMAIL_REGEX.match(log_email):
-            messages.warning(request, "Invalid Email Address!")
-            return redirect('/')
-        elif len(log_check) == 0:
-            messages.warning(request, "Email not found!")
-            return redirect('/')
+        errors = User.objects.email_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('sign')
+        errors = User.objects.dbcheck_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('sign')
+        errors = User.objects.hpw_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('sign')
+        else:
+            #sets logged in user id for use later
+            user = User.objects.get(email=request.POST['email'])
+            request.session['id'] = user.id
+            #sets First name for user interaction
+            request.session['fname'] = user.first_name
+            #sets permission indicator
+            if user.level == "admin":
+                request.session['level'] = 1
+            else:
+                request.session['level'] = 0
+            #redirects to success to display sucessful login
+            return redirect('dash')
 
-        log_pass = User.objects.get(email=log_email).password
+# DASHBOARD - USER *************************************************
 
-        #password validation - compares hashed logged in password to hashed Db password
-        if not bcrypt.checkpw(request.POST['logpassword'].encode(), log_pass.encode()):
-            messages.warning(request, "Password does not match.")
-            return redirect('/')
+def dashboard(request):
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'userdash/dashboard.html', context)
 
-        #sets logged in user id for use later  
-        request.session['id'] = User.objects.get(email=log_email).id
-        #sets First name for user interaction
-        request.session['fname'] = User.objects.get(email=log_email).first_name
-        #sets login or registration indicator
-        request.session['from'] = 1
-        #redirects to success to display sucessful login
+# DASHBOARD - ADMIN *************************************************
+
+def admin(request):
     return redirect('dash')
 
-# SUCCESSFUL LOGIN OR REGISTRATION *************************************************
+# EDIT - ADMIN *************************************************
 
-def success(request):
-    if request.session['from'] == 1:
-        messages.success(request, "Welcome Back {}!  You have successfully logged in!".format(request.session['fname']))
-    else:
-        messages.success(request, "Welcome {}!  You have successfully registered on our site!".format(request.session['fname']))
-    return render(request, 'logreg/success.html')
+def edit(request, id):
+    context = {
+        'user': User.objects.get(id=id)
+    }
+    return render(request, 'userdash/dashboard/edit.html', context)
+
+# EDIT - USER *************************************************
+
+def profile(request):
+    context = {
+        'user': User.objects.get(id=request.session['id'])
+    }
+    return render(request, 'userdash/users/profile.html', context)
+
+# NEW - ADMIN *************************************************
+
+def new(request):
+    return render(request, 'userdash/users/new.html')
+
+# SHOW - USER *************************************************
+
+def show(request, id):
+    this_user = User.objects.get(id=id)
+    print "this_user", this_user
+    context = {
+        'user': this_user,
+        'user_posts': this_user.posts.all(),
+        'comments': this_user.comments.all()
+    }
+    return render(request, 'userdash/users/show.html', context)
+
+# UPDATE - INFO *************************************************
+
+def update(request):
+    if request.method == "POST":
+        errors = User.objects.email_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('profile')
+        errors = User.objects.name_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('profile')
+        errors = User.objects.db_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('profile')
+        else:
+            #Updates validated record in database for user profile
+            update_info = User.objects.get(id=request.POST['id'])
+            update_info.first_name = request.POST['fname']
+            update_info.last_name = request.POST['lname']
+            update_info.email = request.POST['email']
+            update_info.level = request.POST['level']
+            update_info.save()
+            #redirects to success to display sucessful login
+            return redirect('dash')
+
+# UPDATE - PROFILE DESCRIPTION *************************************************
+
+def save(request):
+    if request.method == "POST":
+            #Updates description in database for user profile
+            update_info = User.objects.get(id=request.POST['id'])
+            update_info.desc = request.POST['desc']
+            update_info.save()
+            #redirects to dashboard
+            return redirect('dash')
+
+# CHANGE - PASSWORD *************************************************
+
+def change(request):
+    if request.method == "POST":
+        errors = User.objects.pw_validator(request.POST)
+        if errors is not None:
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('profile')
+        else:
+            hashed_password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt(13))
+            #Updates validated password in database for user profile
+            change_pw = User.objects.get(id=request.POST['id'])
+            change_pw.password = hashed_password
+            change_pw.save()
+            #redirects to success to display sucessful login
+            return redirect('dash')
+
+# DESTROY - ADMIN *************************************************
+
+def destroy(request, id):
+    destroyit = User.objects.get(id=id)
+    destroyit.delete()
+    return redirect('dash')
+
+# LOGOFF *************************************************
+
+def logoff(request):
+    request.session['id'] = 0
+    request.session['fname'] = ''
+    return redirect('/')
